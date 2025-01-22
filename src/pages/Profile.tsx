43d4b2b71a44128415550096c5b42ee51e9a43a9
tsxdudu <141635170+tsxdudu@users.noreply.github.com>
2025-01-22 14:27:06 -0300

    Update Profile.tsx

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -53,15 +53,16 @@ const Profile = () => {
 
       {/* Tags */}
       <div className="flex flex-wrap gap-4 mb-8 z-10 justify-center">
-        <span className="flex items-center gap-2 px-4 py-1 rounded-full bg-[#7E69AB]/50 text-white text-sm">
-          <img src="/programador.png" alt="Programador Logo" className="w-5 h-5" />
-          Programador
-        </span>
-        <span className="flex items-center gap-2 px-4 py-1 rounded-full bg-[#7E69AB]/50 text-white text-sm">
-          <img src="/ciber-seguranca.png" alt="Cybersecurity Logo" className="w-5 h-5" />
-          Cybersecurity
-        </span>
-      </div>
+  <span className="flex items-center gap-2 px-4 py-1 rounded-full bg-black text-white border-2 border-white shadow-[0_0_8px_rgba(255,255,255,0.8)] text-sm">
+    <img src="/programador.png" alt="Programador Logo" className="w-5 h-5" />
+    Programador
+  </span>
+  <span className="flex items-center gap-2 px-4 py-1 rounded-full bg-black text-white border-2 border-white shadow-[0_0_8px_rgba(255,255,255,0.8)] text-sm">
+    <img src="/ciber-seguranca.png" alt="Cybersecurity Logo" className="w-5 h-5" />
+    Cybersecurity
+  </span>
+</div>
+
 
       <div className="flex flex-col items-center gap-4 w-full max-w-md px-4 z-10">
         {/* Redes sociais */}
