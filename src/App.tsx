a8064d4defe8be108a756473ab9c1859045e0e14
tsxdudu <141635170+tsxdudu@users.noreply.github.com>
2025-01-22 02:13:29 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,39 +2,27 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route, Navigate, useLocation } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Click from "./pages/Click";
 import Profile from "./pages/Profile";
 
 const queryClient = new QueryClient();
 
-const App = () => {
-  const location = useLocation();
-
-  // Usamos um useEffect para verificar quando o componente é montado e redirecionar.
-  React.useEffect(() => {
-    if (location.pathname === '/') {
-      // Redireciona para a página /click
-      window.location.replace('/click');
-    }
-  }, [location.pathname]);
-
-  return (
-    <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <Toaster />
-        <Sonner />
-        <BrowserRouter>
-          <Routes>
-            <Route path="/click" element={<Click />} />
-            <Route path="/profile" element={<Profile />} />
-            {/* Definindo a rota raíz como um redirect para /click */}
-            <Route path="/" element={<Navigate to="/click" replace />} />
-          </Routes>
-        </BrowserRouter>
-      </TooltipProvider>
-    </QueryClientProvider>
-  );
-};
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/click" element={<Click />} />
+          <Route path="/profile" element={<Profile />} />
+          {/* Forçar o redirecionamento diretamente para /click */}
+          <Route path="/" element={<Navigate to="/click" />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
 
 export default App;
