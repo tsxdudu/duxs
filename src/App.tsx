771d9a424c36a3259c525c1fdf1c44dc67c06939
tsxdudu <141635170+tsxdudu@users.noreply.github.com>
2025-01-22 02:09:46 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Click from "./pages/Click";
 import Profile from "./pages/Profile";
 
@@ -15,7 +15,9 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Click />} />
+          {/* Garantir que a raiz sempre redirecione para /click */}
+          <Route path="/" element={<Navigate to="/click" />} />
+          <Route path="/click" element={<Click />} />
           <Route path="/profile" element={<Profile />} />
         </Routes>
       </BrowserRouter>
