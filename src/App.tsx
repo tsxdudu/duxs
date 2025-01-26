1db5a870f9d50d6e6d7bd50d275ca955a6558234
tsxdudu <141635170+tsxdudu@users.noreply.github.com>
2025-01-25 21:15:13 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route, Navigate, useNavigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
 import Click from "./pages/Click";
 import Profile from "./pages/Profile";
 
@@ -23,20 +23,6 @@ const RedirectOnLoad = () => {
   return null;
 };
 
-const NotFoundPage = () => (
-  <div
-    style={{
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      height: "100vh",
-      textAlign: "center",
-    }}
-  >
-    <h1 style={{ fontSize: "3rem" }}>thanks for viewing my profile<3 </h1>
-  </div>
-);
-
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
@@ -47,7 +33,6 @@ const App = () => (
         <Routes>
           <Route path="/click" element={<Click />} />
           <Route path="/profile" element={<Profile />} />
-          <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
